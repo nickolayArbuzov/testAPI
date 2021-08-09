0af1831788c2b7f332f0fb167f216b7e1e5fc0f4
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2021-08-09 15:21:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const config = require("config");
 const corsMiddleware = require("./middleware/cors.middleware");
+const testAPIRouter = require("./routes/testAPI.routes");
 
 const app = express();
 const PORT = config.get("serverPort");
 
 app.use(corsMiddleware);
 app.use(express.json());
+app.use("/api", testAPIRouter);
 
 const start = async () => {
     try {
