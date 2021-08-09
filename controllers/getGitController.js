22e0838629e74fc7009b0c312b6a05ba728b8fbc
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2021-08-09 17:42:39 +0300

    Update getGitController.js

--- controllers/getGitController.js
+++ controllers/getGitController.js
@@ -3,9 +3,7 @@ const axios = require('axios');
 class GetGitController {
     async getGit(req, res) {
         try {
-            const { data: { data } } = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=${process.env.API_KEY}`, {
-
-            });
+            const { data: { data } } = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=${process.env.API_KEY}`);
             
             return res.json({
                 data
