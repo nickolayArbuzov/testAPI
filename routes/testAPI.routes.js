e126cc1ff5878d2d7685fe6b5acb937845978e64
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2021-08-09 14:59:23 +0300

    Update testAPI.routes.js

--- routes/testAPI.routes.js
+++ routes/testAPI.routes.js
@@ -3,7 +3,6 @@ const Router = require("express");
 const router = new Router();
 
 router.get('/', (req, res, next) => {
-    debugger
     axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=${process.env.API_KEY}`).
     then(response => res.send(response)).
     catch (e => res.send(e)) ;
